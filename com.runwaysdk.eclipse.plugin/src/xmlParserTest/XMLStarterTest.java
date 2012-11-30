c36a4505d28ba81b82237b1eacf3b26fa8e42f9c
Andrew Miller <andrewrm@colorado.edu>
2012-11-29 17:35:29 -0700

    Prototype for meta Runway objects. SAX XML parsing is still underway. see http://www.java-samples.com/showtutorial.php?tutorialid=152

--- com.runwaysdk.eclipse.plugin/src/xmlParserTest/XMLStarterTest.java
+++ com.runwaysdk.eclipse.plugin/src/xmlParserTest/XMLStarterTest.java
@@ -5,14 +5,6 @@ import java.io.IOException;
 import  javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import org.xml.sax.Attributes;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.File;
-import java.io.Writer;
-import java.io.FileNotFoundException;
-
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -36,67 +28,9 @@ public class XMLStarterTest {
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
 
-			DefaultHandler handler = new DefaultHandler() {
+			DefaultHandler handler = new RunwayXMLHandler();
 
-				Writer writer = null;
-
-
-
-				public void startElement(String uri, String localName,String qName, 
-						Attributes attributes) throws SAXException {
-					try {
-						File file = new File("/Users/armiller5/Documents/Runway-Eclipse-Plugin/XMLPARSE.txt");
-						writer = new BufferedWriter(new FileWriter(file));
-
-
-						System.out.println("Start Element :" + qName);
-
-						if (qName.equalsIgnoreCase("FIRSTNAME")) {
-							writer.write("Found this element!/n");
-						}
-
-						if (qName.equalsIgnoreCase(XMLTags.MD_BUSINESS_TAG)) {
-							writer.write("HERE IS OUR BUSINESS TAG!!/n");
-						}
-
-						if (qName.equalsIgnoreCase("NICKNAME")) {
-							writer.write("Found this element!/n");
-						}
-					}
-					catch (FileNotFoundException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					finally {
-						try {
-							if (writer != null) {
-								writer.close();
-							}
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-
-				}
-
-
-				public void endElement(String uri, String localName,
-						String qName) throws SAXException {
-
-					System.out.println("End Element :" + qName);
-
-				}
-
-				public void characters(char ch[], int start, int length) throws SAXException {
-
-					System.out.println("-----Tag value----------->"+new String(ch, start, length));
-
-				}
-
-			};
-
-			saxParser.parse("c:\\file.xml", handler);
+			saxParser.parse("/Users/armiller5/Documents/workspace/RunwayTemplate/doc/individual/schema(0001352140861497)HelloWorld.xml", handler);
 
 		} catch (Exception e) {
 			e.printStackTrace();
