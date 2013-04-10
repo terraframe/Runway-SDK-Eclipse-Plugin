10337774a0631d5e4446bd0ac0d0964f761aed74
rrowlands <rrowlands@terraframe.com>
2013-04-09 19:18:14 -0600

    Short refactor of Terek's exporter code.

--- com.runwaysdk.eclipse.plugin.diagram/customsrc/com/runwaysdk/eclipse/plugin/schema/exporter/DOMExporter.java
+++ com.runwaysdk.eclipse.plugin.diagram/customsrc/com/runwaysdk/eclipse/plugin/schema/exporter/DOMExporter.java
@@ -1,6 +1,9 @@
 package com.runwaysdk.eclipse.plugin.schema.exporter;
+
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,238 +17,261 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.eclipse.core.runtime.Platform;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import com.runwaysdk.eclipse.plugin.schema.runwayxml.XMLMdBusiness;
 import com.runwaysdk.eclipse.plugin.schema.runwayxml.XMLMetadata;
 
-public class DOMExporter {
-	
-    private Document dom;
-    private Element version;
-    private Element doIt;
-    private Element doItCreate;
-    private Element doItUpdate;
-    private Element doItDelete;
-    private Element undoIt;
-    private Element undoItCreate;
-    private Element undoItUpdate;
-    private Element undoItDelete;
+public class DOMExporter
+{
 
-	public static void main(String[] args) {
-		
-		List<XmlAttribute> attributes = new ArrayList<XmlAttribute>();
-		List<XmlElement> elements = new ArrayList<XmlElement>();
-		
-		XmlAttribute attTest = new XmlAttribute("name", "value");
-		XmlAttribute attTest2 = new XmlAttribute("name2", "value2");
-		attributes.add(attTest);
-		attributes.add(attTest2);
-		
-		XmlElement eleTest =  new XmlElement("XmlName", null, attributes);
-		XmlElement eleTest2 =  new XmlElement("XmlName2", eleTest, attributes);
-		elements.add(eleTest);
-		elements.add(eleTest2);
-		
-		System.out.println(elements.size());
+  private Document dom;
 
-		String fileName = "garbledMen.xml";
-//		writeMdBusiness(fileName, elements);
-		
-	}
-	
-	// This method is called when the user saves the document.
-	public static void doExport() {
-		// Generates an empty Runway XML file
-		DOMExporter instance = new DOMExporter();
-		String fileName = "garbledMen";
-		instance.generateEmptySchema(fileName);
-		
-		List<XMLMdBusiness> records = XMLRecordFactory.getRecords();
-		for (int i = 0; i < records.size(); i++) {
-			XMLMdBusiness record = records.get(i);
-			Element el = record.writeDoItXML(instance.dom);
-			if (record.getCrudFlag() == XMLMetadata.CREATE) {
-				el.appendChild(instance.doItCreate);
-			} else if (record.getCrudFlag() == XMLMetadata.UPDATE) {
-				el.appendChild(instance.doItUpdate);
-			} else if (record.getCrudFlag() == XMLMetadata.DELETE) {
-				el.appendChild(instance.doItDelete);
-			}
-		}
-        try {
-            Transformer tr = TransformerFactory.newInstance().newTransformer();
-            tr.setOutputProperty(OutputKeys.INDENT, "yes");
-            tr.setOutputProperty(OutputKeys.METHOD, "xml");
-            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            tr.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+  private Element  version;
 
-            // send DOM to file
-            tr.transform(new DOMSource(instance.dom), 
-                                 new StreamResult(new FileOutputStream(fileName)));
-        } catch (TransformerException te) {
-            System.out.println(te.getMessage());
-        } catch (IOException ioe) {
-            System.out.println(ioe.getMessage());
-        }
+  private Element  doIt;
+
+  private Element  doItCreate;
+
+  private Element  doItUpdate;
+
+  private Element  doItDelete;
+
+  private Element  undoIt;
+
+  private Element  undoItCreate;
+
+  private Element  undoItUpdate;
+
+  private Element  undoItDelete;
+
+  public static void main(String[] args)
+  {
+
+    List<XmlAttribute> attributes = new ArrayList<XmlAttribute>();
+    List<XmlElement> elements = new ArrayList<XmlElement>();
+
+    XmlAttribute attTest = new XmlAttribute("name", "value");
+    XmlAttribute attTest2 = new XmlAttribute("name2", "value2");
+    attributes.add(attTest);
+    attributes.add(attTest2);
+
+    XmlElement eleTest = new XmlElement("XmlName", null, attributes);
+    XmlElement eleTest2 = new XmlElement("XmlName2", eleTest, attributes);
+    elements.add(eleTest);
+    elements.add(eleTest2);
+
+    System.out.println(elements.size());
+
+    String fileName = "garbledMen.xml";
+    // writeMdBusiness(fileName, elements);
+
+  }
+
+  // This method is called when the user saves the document.
+  public static void doExport()
+  {
+    URL url = Platform.getInstanceLocation().getURL();
+    String location = new File(url.getPath()).getAbsolutePath();
+    
+    String fileName = location + "/garbledMen";
+    
+    System.out.println("Writing file to '" + fileName + "'");
+
+    // Generates an empty Runway XML file
+    DOMExporter instance = new DOMExporter();
+    instance.generateEmptySchema(fileName);
+
+    List<XMLMdBusiness> records = XMLRecordFactory.getRecords();
+    for (int i = 0; i < records.size(); i++)
+    {
+      XMLMdBusiness record = records.get(i);
+      
+      Element el = record.writeDoItXML(instance.dom);
+      
+      if (record.getCrudFlag() == XMLMetadata.CREATE)
+      {
+        el.appendChild(instance.doItCreate);
+      }
+      else if (record.getCrudFlag() == XMLMetadata.UPDATE)
+      {
+        el.appendChild(instance.doItUpdate);
+      }
+      else if (record.getCrudFlag() == XMLMetadata.DELETE)
+      {
+        el.appendChild(instance.doItDelete);
+      }
     }
+    try
+    {
+      Transformer tr = TransformerFactory.newInstance().newTransformer();
+      tr.setOutputProperty(OutputKeys.INDENT, "yes");
+      tr.setOutputProperty(OutputKeys.METHOD, "xml");
+      tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+      tr.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+      tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
-
-    static Document elementBuilder(Document dom, String elementName, List<XmlAttribute> attributeList, XmlElement parent) {
-    	Element elementBuilder = dom.createElement(elementName);
-    	for (int j = 0; j < attributeList.size(); j++) {
-    		elementBuilder.setAttribute(attributeList.get(j).getAttributeName(),
-    				attributeList.get(j).getAttributeValue());
-    	}
-    	if (parent != null) {
-    		// i need to change parent from XmlElement to Element
- //   		elementBuilder.getNodeName(); // Just test code
-    		
-    		String parentName = parent.getElementName(); // Get's the parents name
-
-    		//Where do I set the parents id?
-    		//This assumes that the node's name is its id. Which is false.
-    		Element parentId = dom.getElementById(parentName); // Get's the parents unique id?
-
-    		parentId.appendChild(elementBuilder); // Should append the child to the parent in the dom
-    		
-    	} else {
-    		// Declares the Element as the root node 
-   			dom.appendChild(elementBuilder);
-    	}
-    	return dom;
+      // send DOM to file
+      tr.transform(new DOMSource(instance.dom), new StreamResult(new FileOutputStream(fileName)));
     }
+    catch (TransformerException te)
+    {
+      System.out.println(te.getMessage());
+    }
+    catch (IOException ioe)
+    {
+      System.out.println(ioe.getMessage());
+    }
+  }
 
-	public void generateEmptySchema (String filename) {
-	    Document dom;
+  static Document elementBuilder(Document dom, String elementName, List<XmlAttribute> attributeList,
+      XmlElement parent)
+  {
+    Element elementBuilder = dom.createElement(elementName);
+    for (int j = 0; j < attributeList.size(); j++)
+    {
+      elementBuilder.setAttribute(attributeList.get(j).getAttributeName(), attributeList.get(j)
+          .getAttributeValue());
+    }
+    if (parent != null)
+    {
+      // i need to change parent from XmlElement to Element
+      // elementBuilder.getNodeName(); // Just test code
 
-	    // instance of a DocumentBuilderFactory
-	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	    try {
-	        // use factory to get an instance of document builder
-	        DocumentBuilder db = dbf.newDocumentBuilder();
-	        // create instance of DOM
-	        dom = db.newDocument();
-	        
-	        // create data elements and place them in the structure
-	        version = dom.createElement("version");
-	        version.setAttribute("xsi:noNamespaceSchemaLocation", "../../profiles/version_gis.xsd");
-	        version.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+      String parentName = parent.getElementName(); // Get's the parents name
 
-	        doIt = dom.createElement("doIt");
-	        version.appendChild(doIt);
+      // Where do I set the parents id?
+      // This assumes that the node's name is its id. Which is false.
+      Element parentId = dom.getElementById(parentName); // Get's the parents
+                                                         // unique id?
 
-	        doItCreate = dom.createElement("create");
-	        doIt.appendChild(doItCreate);
+      parentId.appendChild(elementBuilder); // Should append the child to the
+                                            // parent in the dom
 
-	        doItUpdate = dom.createElement("update");
-	        doIt.appendChild(doItUpdate);
+    }
+    else
+    {
+      // Declares the Element as the root node
+      dom.appendChild(elementBuilder);
+    }
+    return dom;
+  }
 
-	        doItDelete = dom.createElement("delete");
-	        doIt.appendChild(doItDelete);
+  public void generateEmptySchema(String filename)
+  {
+    // instance of a DocumentBuilderFactory
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    try
+    {
+      // use factory to get an instance of document builder
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      // create instance of DOM
+      dom = db.newDocument();
 
-	        undoIt = dom.createElement("undoIt");
-	        version.appendChild(undoIt);
+      // create data elements and place them in the structure
+      version = dom.createElement("version");
+      version.setAttribute("xsi:noNamespaceSchemaLocation", "../../profiles/version_gis.xsd");
+      version.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
 
-	        undoItCreate = dom.createElement("create");
-	        doIt.appendChild(undoItCreate);
+      doIt = dom.createElement("doIt");
+      version.appendChild(doIt);
 
-	        undoItUpdate = dom.createElement("update");
-	        doIt.appendChild(undoItUpdate);
+      doItCreate = dom.createElement("create");
+      doIt.appendChild(doItCreate);
 
-	        undoItDelete = dom.createElement("delete");
-	        undoIt.appendChild(undoItDelete);
+      doItUpdate = dom.createElement("update");
+      doIt.appendChild(doItUpdate);
 
-	        dom.appendChild(version);
+      doItDelete = dom.createElement("delete");
+      doIt.appendChild(doItDelete);
 
-	    } catch (ParserConfigurationException pce) {
-	        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
-	    }
-	}
+      undoIt = dom.createElement("undoIt");
+      version.appendChild(undoIt);
 
-	
-    
-    
-    
-    
-/*	public static void writeMdBusiness(String filename, String name, String display) {
-	    Document dom;
+      undoItCreate = dom.createElement("create");
+      doIt.appendChild(undoItCreate);
 
-	    // instance of a DocumentBuilderFactory
-	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	    try {
-	        // use factory to get an instance of document builder
-	        DocumentBuilder db = dbf.newDocumentBuilder();
-	        // create instance of DOM
-	        dom = db.newDocument();
-	        
-	        // create data elements and place them in the structure
-	        Element version = dom.createElement("version");
-	        version.setAttribute("xsi:noNamespaceSchemaLocation", "../../profiles/version_gis.xsd");
-	        version.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+      undoItUpdate = dom.createElement("update");
+      doIt.appendChild(undoItUpdate);
 
-	        Element doIt = dom.createElement("doIt");
-	        version.appendChild(doIt);
+      undoItDelete = dom.createElement("delete");
+      undoIt.appendChild(undoItDelete);
 
-	        Element create = dom.createElement("create");
-	        doIt.appendChild(create);
+      dom.appendChild(version);
 
-	        Element mdBusiness = dom.createElement("mdBusiness");
-	        mdBusiness.setAttribute("name", "com.terraframe.runwaytemplate.HelloWorld");
-	        mdBusiness.setAttribute("label", "HelloWorld");
-	        create.appendChild(mdBusiness);
+    }
+    catch (ParserConfigurationException pce)
+    {
+      System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+    }
+  }
 
-	        Element attributes = dom.createElement("attributes");
-	        mdBusiness.appendChild(attributes);
-	        
-	        Element character = dom.createElement("char");
-	        character.setAttribute("name", "greeting");
-	        character.setAttribute("required", "true");
-	        character.setAttribute("label", "Greeting");
-	        character.setAttribute("size", "50");
-	        attributes.appendChild(character);
+  /*
+   * public static void writeMdBusiness(String filename, String name, String
+   * display) { Document dom;
+   * 
+   * // instance of a DocumentBuilderFactory DocumentBuilderFactory dbf =
+   * DocumentBuilderFactory.newInstance(); try { // use factory to get an
+   * instance of document builder DocumentBuilder db = dbf.newDocumentBuilder();
+   * // create instance of DOM dom = db.newDocument();
+   * 
+   * // create data elements and place them in the structure Element version =
+   * dom.createElement("version");
+   * version.setAttribute("xsi:noNamespaceSchemaLocation",
+   * "../../profiles/version_gis.xsd"); version.setAttribute("xmlns:xsi",
+   * "http://www.w3.org/2001/XMLSchema-instance");
+   * 
+   * Element doIt = dom.createElement("doIt"); version.appendChild(doIt);
+   * 
+   * Element create = dom.createElement("create"); doIt.appendChild(create);
+   * 
+   * Element mdBusiness = dom.createElement("mdBusiness");
+   * mdBusiness.setAttribute("name",
+   * "com.terraframe.runwaytemplate.HelloWorld");
+   * mdBusiness.setAttribute("label", "HelloWorld");
+   * create.appendChild(mdBusiness);
+   * 
+   * Element attributes = dom.createElement("attributes");
+   * mdBusiness.appendChild(attributes);
+   * 
+   * Element character = dom.createElement("char");
+   * character.setAttribute("name", "greeting");
+   * character.setAttribute("required", "true"); character.setAttribute("label",
+   * "Greeting"); character.setAttribute("size", "50");
+   * attributes.appendChild(character);
+   * 
+   * Element mdMethod = dom.createElement("mdMethod");
+   * mdBusiness.appendChild(mdMethod);
+   * 
+   * Element update = dom.createElement("update"); doIt.appendChild(update);
+   * 
+   * Element undoIt = dom.createElement("undoIt"); version.appendChild(undoIt);
+   * 
+   * Element delete = dom.createElement("delete"); undoIt.appendChild(delete);
+   * 
+   * Element object = dom.createElement("object"); object.setAttribute("key",
+   * "com.terraframe.runwaytemplate.HelloWorld"); object.setAttribute("type",
+   * "com.runwaysdk.system.metadata.MdBusiness"); delete.appendChild(object);
+   * 
+   * dom.appendChild(version);
+   * 
+   * try { Transformer tr = TransformerFactory.newInstance().newTransformer();
+   * tr.setOutputProperty(OutputKeys.INDENT, "yes");
+   * tr.setOutputProperty(OutputKeys.METHOD, "xml");
+   * tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+   * tr.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+   * tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+   * 
+   * // send DOM to file tr.transform(new DOMSource(dom), new StreamResult(new
+   * FileOutputStream(filename)));
+   * 
+   * } catch (TransformerException te) { System.out.println(te.getMessage()); }
+   * catch (IOException ioe) { System.out.println(ioe.getMessage()); } } catch
+   * (ParserConfigurationException pce) {
+   * System.out.println("UsersXML: Error trying to instantiate DocumentBuilder "
+   * + pce); } }
+   */
 
-	        Element mdMethod = dom.createElement("mdMethod");
-	        mdBusiness.appendChild(mdMethod);
-
-	        Element update = dom.createElement("update");
-	        doIt.appendChild(update);
-
-	        Element undoIt = dom.createElement("undoIt");
-	        version.appendChild(undoIt);
-
-	        Element delete = dom.createElement("delete");
-	        undoIt.appendChild(delete);
-
-	        Element object = dom.createElement("object");
-	        object.setAttribute("key", "com.terraframe.runwaytemplate.HelloWorld");
-	        object.setAttribute("type", "com.runwaysdk.system.metadata.MdBusiness");
-	        delete.appendChild(object);
-
-	        dom.appendChild(version);
-
-	        try {
-	            Transformer tr = TransformerFactory.newInstance().newTransformer();
-	            tr.setOutputProperty(OutputKeys.INDENT, "yes");
-	            tr.setOutputProperty(OutputKeys.METHOD, "xml");
-	            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-	            tr.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-	            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-	            // send DOM to file
-	            tr.transform(new DOMSource(dom), 
-	                                 new StreamResult(new FileOutputStream(filename)));
-
-	        } catch (TransformerException te) {
-	            System.out.println(te.getMessage());
-	        } catch (IOException ioe) {
-	            System.out.println(ioe.getMessage());
-	        }
-	    } catch (ParserConfigurationException pce) {
-	        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
-	    }
-	} */
-	
 }
--- com.runwaysdk.eclipse.plugin.diagram/customsrc/com/runwaysdk/eclipse/plugin/schema/runwayxml/XMLMetadata.java
+++ com.runwaysdk.eclipse.plugin.diagram/customsrc/com/runwaysdk/eclipse/plugin/schema/runwayxml/XMLMetadata.java
@@ -12,59 +12,72 @@ import com.runwaysdk.eclipse.plugin.runway.MetaData;
 abstract public class XMLMetadata
 {
   private Map<String, String> xmlAttributes = new HashMap<String, String>();
-  
-  private MetaData metadata;
-  private Element doItExport, undoItExport;
-  
-public static final int UNMODIFIED = 0;
-  public static final int CREATE = 1;
-  public static final int UPDATE = 2;
-  public static final int DELETE = 3;
-  private int crudFlag = UNMODIFIED;
-  
-  public XMLMetadata() {
-    
+
+  private MetaData            metadata;
+
+  private Element             doItExport, undoItExport;
+
+  public static final int     UNMODIFIED    = 0;
+
+  public static final int     CREATE        = 1;
+
+  public static final int     UPDATE        = 2;
+
+  public static final int     DELETE        = 3;
+
+  private int                 crudFlag      = UNMODIFIED;
+
+  public XMLMetadata()
+  {
+
   }
-  
-  public void setXMLAttribute(String key, String value) {
+
+  public void setXMLAttribute(String key, String value)
+  {
     xmlAttributes.put(key, value);
   }
-  
-  public String getXMLAttribute(String key) {
+
+  public String getXMLAttribute(String key)
+  {
     return xmlAttributes.get(key);
   }
-  
-  public MetaData readFromXML(NamedNodeMap nodeMap) {
+
+  public MetaData readFromXML(NamedNodeMap nodeMap)
+  {
     MetaData metadata = getMetadata();
-    
+
     // TODO : set attributes
-    
+
     return metadata;
   }
-  
-  public Element writeDoItXML(Document dom) {
+
+  public Element writeDoItXML(Document dom)
+  {
     Element xml = doItExport;
-    
+
     Object[] keys = xmlAttributes.keySet().toArray();
-    for (int i = 0; i < keys.length; ++i) {
+    for (int i = 0; i < keys.length; ++i)
+    {
       String key = (String) keys[i];
       xml.setAttribute(key, xmlAttributes.get(key));
     }
-    
+
     return xml;
   }
-  
-  public Element writeUndoItXML() {
-	    Element xml = doItExport;
-	    
-	    Object[] keys = xmlAttributes.keySet().toArray();
-	    for (int i = 0; i < keys.length; ++i) {
-	      String key = (String) keys[i];
-	      xml.setAttribute(key, xmlAttributes.get(key));
-	    }
-	    
-	    return xml;
-	  }
+
+  public Element writeUndoItXML()
+  {
+    Element xml = doItExport;
+
+    Object[] keys = xmlAttributes.keySet().toArray();
+    for (int i = 0; i < keys.length; ++i)
+    {
+      String key = (String) keys[i];
+      xml.setAttribute(key, xmlAttributes.get(key));
+    }
+
+    return xml;
+  }
 
   public MetaData getMetadata()
   {
@@ -75,7 +88,7 @@ public static final int UNMODIFIED = 0;
   {
     this.metadata = mdBiz;
   }
-  
+
   public int getCrudFlag()
   {
     return crudFlag;
@@ -85,22 +98,25 @@ public static final int UNMODIFIED = 0;
   {
     this.crudFlag = crudFlag;
   }
-  
-  public Element getDoItExport() {
-	return doItExport;
-}
 
-public void setDoItExport(Element doItExport) {
-	this.doItExport = doItExport;
-}
+  public Element getDoItExport()
+  {
+    return doItExport;
+  }
 
-public Element getUndoItExport() {
-	return undoItExport;
-}
+  public void setDoItExport(Element doItExport)
+  {
+    this.doItExport = doItExport;
+  }
 
-public void setUndoItExport(Element undoItExport) {
-	this.undoItExport = undoItExport;
-}
+  public Element getUndoItExport()
+  {
+    return undoItExport;
+  }
 
+  public void setUndoItExport(Element undoItExport)
+  {
+    this.undoItExport = undoItExport;
+  }
 
 }
